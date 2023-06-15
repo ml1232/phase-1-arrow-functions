e40cc1b061bc0cb3842838dcb9bf5c1ef218953a
Matthew <matthewjaredlevin88@gmail.com>
2023-06-15 12:24:28 -0500

    Complete Arrow Functions Lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(a, b) {
+    return a / b;
+  }
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
